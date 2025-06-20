c0fac6a086781d1355c9f4c55e42aae7282b8c0d
elouannh <elouannhosta27@gmail.com>
2025-06-20 17:39:26 +0200

    Trying debugging the CI/CD

--- scripts/clear_repo_develop.sh
+++ scripts/clear_repo_develop.sh
@@ -2,7 +2,9 @@ echo -e "\e[48;2;10;240;10;m//////////////////////////////////\e[0m\n"
 echo -e "\e[38;2;10;240;10;mCleaning repository for develop...\e[0m\n"
 echo -e "\e[48;2;10;240;10;m//////////////////////////////////\e[0m\n"
 
+ls -lR
 make fclean 1> /dev/null
+ls -lR
 
 echo -e "\e[48;2;10;240;10;m//////////////////////////////////\e[0m\n"
 echo -e "\e[38;2;10;240;10;mRepo cleaned!\e[0m\n"
@@ -12,6 +14,7 @@ echo -e "\e[48;2;10;240;240;m//////////////////////////////////\e[0m\n"
 echo -e "\e[38;2;10;240;240;mPushing now...\e[0m\n"
 echo -e "\e[48;2;10;240;240;m//////////////////////////////////\e[0m\n"
 
+git status --porcelain
 if [[ -n "$(git status --porcelain)" ]]; then
   git config user.name "github-actions[bot]"
   git config user.email "github-actions[bot]@users.noreply.github.com"
@@ -21,6 +24,7 @@ if [[ -n "$(git status --porcelain)" ]]; then
 else
   echo -e "\e[38;2;240;10;10;mNothing has to be committed.\e[0m\n"
 fi
+ls -lR
 
 echo -e "\e[48;2;10;240;240;m//////////////////////////////////\e[0m\n"
 echo -e "\e[38;2;10;240;240;mEverything has been pushed!\e[0m\n"
